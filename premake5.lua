a69f70982f76821442107b7e64b53c7e5ea21142
Stas <morstas99@mail.ru>
2022-02-09 19:22:52 +0300

    staticruntime om

--- premake5.lua
+++ premake5.lua
@@ -3,7 +3,7 @@ project "ImGui"
 	language "C++"
 	cppdialect "C++17"
     	staticruntime "on"
-
+ 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
